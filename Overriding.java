1c12d2be26c232c043c8b59141fed06796d23985
Jim Teresco <terescoj@gmail.com>
2020-02-02 09:28:40 -0500

    Added example.

---
+++ Overriding.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+
+/**
+ * Example demonstrating overriding of methods in subclasses
+ *
+ * @author Jim Teresco, 
+ * Siena College, Computer Science 225, Spring 2017
+ * Based heavily on example from Darren Lim
+ * @version Spring 2020
+ */
+
+class Student
+{
+    public String name = "Fred";
+    
+    protected void getName()
+    {
+        System.out.println(name);
+    }       
+}
+
+
+class Undergraduate extends Student
+{
+    @Override
+    protected void getName()
+    {
+        System.out.println("Undergraduate : " + name);
+    }
+} 
+
+class Freshman extends Undergraduate
+{
+}
+
+class Graduate extends Student
+{
+    @Override
+    protected void getName()
+    {
+        System.out.println("Graduate : " + name);
+    }
+
+}
+
+class Phd extends Graduate
+{
+    @Override
+    protected void getName()
+    {
+        System.out.println("Phd : " + name);
+    }
+}
+
+public class Overriding 
+{
+
+    public static void main(String[] args) 
+    {
+        // populate an ArrayList with Student objects and instances of derived classes
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student());
+        students.add(new Undergraduate());
+        students.add(new Freshman());
+        students.add(new Graduate());
+        students.add(new Phd());
+        
+        // let's call each one's getName method
+        for (Student s : students) {
+            
+            s.getName();
+        }
+    
+    }
+}
